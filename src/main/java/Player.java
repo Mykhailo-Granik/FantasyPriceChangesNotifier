e9f89bd40f1064101bce9c6d03d84f7262d22ca7
Mykhailo Granik <mgranik@axcient.com>
2023-02-12 16:15:57 +0200

    Created Player DTO

---
+++ src/main/java/Player.java
@@ -0,0 +1,17 @@
+public class Player {
+
+    private String name;
+    private String club;
+    private String position;
+    private int price;
+    private int target;
+
+    public Player(String name, String club, String position, double price, double target) {
+        this.name = name;
+        this.club = club;
+        this.position = position;
+        this.price = (int) Math.round(price * 10);
+        this.target = (int) Math.round(target * 10);
+    }
+
+}
