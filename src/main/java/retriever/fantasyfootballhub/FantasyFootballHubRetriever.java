81c9267dd324e46af9801898fb2c4e176d9d4438
Mykhailo Granik <mgranik@axcient.com>
2023-02-25 14:50:34 +0200

    Started implementing Fantasy Football Hub data retriever

---
+++ src/main/java/retriever/fantasyfootballhub/FantasyFootballHubRetriever.java
@@ -0,0 +1,13 @@
+package retriever.fantasyfootballhub;
+
+import common.Player;
+import retriever.Retriever;
+
+import java.util.List;
+
+public class FantasyFootballHubRetriever implements Retriever {
+    @Override
+    public List<Player> retrieve() {
+        return null;
+    }
+}
