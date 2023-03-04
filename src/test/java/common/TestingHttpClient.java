3b835c601a9793d80400e730cd7906e61dbd45c2
Mykhailo Granik <mgranik@axcient.com>
2023-03-04 16:36:31 +0200

    Implemented testing http request

---
+++ src/test/java/common/TestingHttpClient.java
@@ -0,0 +1,85 @@
+package common;
+
+import lombok.Getter;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLParameters;
+import java.net.Authenticator;
+import java.net.CookieHandler;
+import java.net.ProxySelector;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+
+@Getter
+public class TestingHttpClient extends HttpClient {
+
+    private HttpRequest lastSentRequest;
+
+    @Override
+    public Optional<CookieHandler> cookieHandler() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Duration> connectTimeout() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Redirect followRedirects() {
+        return null;
+    }
+
+    @Override
+    public Optional<ProxySelector> proxy() {
+        return Optional.empty();
+    }
+
+    @Override
+    public SSLContext sslContext() {
+        return null;
+    }
+
+    @Override
+    public SSLParameters sslParameters() {
+        return null;
+    }
+
+    @Override
+    public Optional<Authenticator> authenticator() {
+        return Optional.empty();
+    }
+
+    @Override
+    public Version version() {
+        return null;
+    }
+
+    @Override
+    public Optional<Executor> executor() {
+        return Optional.empty();
+    }
+
+    @Override
+    public <T> HttpResponse<T> send(HttpRequest request, HttpResponse.BodyHandler<T> responseBodyHandler) {
+        lastSentRequest = request;
+        return null;
+    }
+
+    @Override
+    public <T> CompletableFuture<HttpResponse<T>> sendAsync(HttpRequest request, HttpResponse.BodyHandler<T> responseBodyHandler) {
+        lastSentRequest = request;
+        return null;
+    }
+
+    @Override
+    public <T> CompletableFuture<HttpResponse<T>> sendAsync(HttpRequest request, HttpResponse.BodyHandler<T> responseBodyHandler, HttpResponse.PushPromiseHandler<T> pushPromiseHandler) {
+        lastSentRequest = request;
+        return null;
+    }
+}
