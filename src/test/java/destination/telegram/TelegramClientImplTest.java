2fbc0591348d3abd28fc16600ddbe94e9ea35a8e
Mykhailo Granik <mgranik@axcient.com>
2023-03-04 19:34:48 +0200

    Implemented TelegramClient

---
+++ src/test/java/destination/telegram/TelegramClientImplTest.java
@@ -0,0 +1,55 @@
+package destination.telegram;
+
+import common.TestingHttpClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import properties.ApplicationProperties;
+
+import java.net.URLEncoder;
+import java.util.Map;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TelegramClientImplTest {
+
+    public static final String TELEGRAM_TOKEN_KEY = "telegram.token";
+    public static final String TELEGRAM_CHAT_ID_KEY = "telegram.chatId";
+    public static final String TELEGRAM_TOKEN_VALUE = "1234567890:ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static final String TELEGRAM_CHAT_ID_VALUE = "123456789";
+    private final TestingHttpClient httpClient = new TestingHttpClient();
+    private TelegramClientImpl underTest;
+
+
+    @BeforeEach
+    void setUp() {
+        underTest = new TelegramClientImpl(new TestingApplicationProperties(), httpClient);
+    }
+
+    @Test
+    public void shouldCorrectlySendTelegramMessage() {
+        String testMessage = "test message";
+        underTest.sendMessage(testMessage);
+        assertEquals(
+                String.format(
+                        "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s",
+                        TELEGRAM_TOKEN_VALUE, TELEGRAM_CHAT_ID_VALUE, URLEncoder.encode(testMessage, UTF_8)
+                ),
+                httpClient.getLastSentRequest().uri().toString()
+        );
+    }
+
+    public static class TestingApplicationProperties implements ApplicationProperties {
+
+        private final Map<String, String> properties =
+                Map.of(
+                        TELEGRAM_TOKEN_KEY, TELEGRAM_TOKEN_VALUE,
+                        TELEGRAM_CHAT_ID_KEY, TELEGRAM_CHAT_ID_VALUE
+                );
+        @Override
+        public String getString(String key) {
+            return properties.get(key);
+        }
+    }
+
+}
