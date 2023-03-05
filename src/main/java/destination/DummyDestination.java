265d5aa79d663f576b8eaad02eb2b6bbca72b1d9
Mykhailo Granik <mgranik@axcient.com>
2023-03-05 19:36:58 +0200

    Refactored DummyDestination

--- src/main/java/destination/DummyDestination.java
+++ src/main/java/destination/DummyDestination.java
@@ -1,19 +1,14 @@
 package destination;
 
 import common.Player;
-import filter.PlayerFilter;
 
 import java.util.List;
 
 
-public class DummyDestination extends AbstractDestination {
-
-    public DummyDestination(PlayerFilter playerFilter) {
-        super(playerFilter);
-    }
+public class DummyDestination implements Destination {
 
     @Override
-    protected void sendFilteredPlayers(List<Player> players) {
+    public void send(List<Player> players) {
         players.forEach(System.out::println);
     }
 }
