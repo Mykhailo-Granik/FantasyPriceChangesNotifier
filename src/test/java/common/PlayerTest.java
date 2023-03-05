1723bfca91e41a95984b219402b7d541f6dc2b45
Mykhailo Granik <mgranik@axcient.com>
2023-03-05 19:22:03 +0200

    Changed target representation in Player.toString

--- src/test/java/common/PlayerTest.java
+++ src/test/java/common/PlayerTest.java
@@ -9,7 +9,7 @@ public class PlayerTest {
     @Test
     public void shouldCorrectlyConvertToString() {
         Player player = new Player("player1", "club1", Position.FWD, 1.0, 2.0);
-        assertEquals("player1(club1), 1.0£, target=2.0", player.toString());
+        assertEquals("player1(club1), 1.0£, 2.0%", player.toString());
     }
 
 }
