684fc6432334daecf5f404b6f38731fb9cfccf10
Mykhailo Granik <mgranik@axcient.com>
2023-03-05 18:00:58 +0200

    Refactored PlayersCloseToPriceFallFilter

--- src/main/java/filter/PlayersCloseToPriceFallFilter.java
+++ src/main/java/filter/PlayersCloseToPriceFallFilter.java
@@ -1,13 +1,15 @@
 package filter;
 
 import common.Player;
-import lombok.RequiredArgsConstructor;
 import util.DoubleWithOnePlace;
 
-@RequiredArgsConstructor
 public class PlayersCloseToPriceFallFilter implements PlayerFilter {
 
     private final DoubleWithOnePlace threshold;
+
+    public PlayersCloseToPriceFallFilter(double threshold) {
+        this.threshold = new DoubleWithOnePlace(threshold);
+    }
 
     @Override
     public boolean test(Player player) {
--- src/test/java/filter/PlayersCloseToPriceFallFilterTest.java
+++ src/test/java/filter/PlayersCloseToPriceFallFilterTest.java
@@ -2,29 +2,29 @@ package filter;
 
 import common.Player;
 import org.junit.jupiter.api.Test;
-import util.DoubleWithOnePlace;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayersCloseToPriceFallFilterTest {
 
     @Test
     public void shouldSayPlayerIsCloseToPriceFallWhenItsTargetIsLessThanThreshold() {
-        PlayersCloseToPriceFallFilter filter = new PlayersCloseToPriceFallFilter(new DoubleWithOnePlace(-95.0));
+        PlayersCloseToPriceFallFilter filter = new PlayersCloseToPriceFallFilter(-95.0);
         Player player = Player.createPlayerWithTarget(-95.1);
         assertTrue(filter.test(player));
     }
 
     @Test
     public void shouldSayPlayerIsCloseToPriceFallWhenItsTargetIsEqualToThreshold() {
-        PlayersCloseToPriceFallFilter filter = new PlayersCloseToPriceFallFilter(new DoubleWithOnePlace(-95.0));
+        PlayersCloseToPriceFallFilter filter = new PlayersCloseToPriceFallFilter(-95.0);
         Player player = Player.createPlayerWithTarget(-95.0);
         assertTrue(filter.test(player));
     }
 
     @Test
     public void shouldSayPlayerIsNotCloseToPriceFallWhenItsTargetIsGreaterThanThreshold() {
-        PlayersCloseToPriceFallFilter filter = new PlayersCloseToPriceFallFilter(new DoubleWithOnePlace(-95.0));
+        PlayersCloseToPriceFallFilter filter = new PlayersCloseToPriceFallFilter(-95.0);
         Player player = Player.createPlayerWithTarget(-94.9);
         assertFalse(filter.test(player));
     }
