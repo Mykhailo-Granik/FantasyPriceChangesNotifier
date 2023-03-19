1067e211477d36d5affce537eccaac8d19f7e687
Mykhailo Granik <mgranik@axcient.com>
2023-03-19 22:16:54 +0200

    Using FPLStatistics retriever

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import destination.telegram.TelegramClientImpl;
 import destination.telegram.TelegramDestination;
 import message.PlayersCloseToPriceFallMessage;
 import message.PlayersCloseToPriceRiseMessage;
+import properties.ApplicationProperties;
 import properties.ApplicationPropertiesImpl;
 import retriever.RetrieverFactory;
 import sender.Sender;
@@ -12,15 +13,16 @@ import java.util.List;
 public class App {
 
     public static void main(String[] args) throws IOException {
+        ApplicationProperties applicationProperties = new ApplicationPropertiesImpl();
         new Sender(
-                new RetrieverFactory().create(),
+                new RetrieverFactory(applicationProperties).create(),
                 List.of(
                         new TelegramDestination(
                                 List.of(
-                                        new PlayersCloseToPriceRiseMessage(95.0),
-                                        new PlayersCloseToPriceFallMessage(-95.0)
+                                        new PlayersCloseToPriceRiseMessage(99.0),
+                                        new PlayersCloseToPriceFallMessage(-99.0)
                                 ),
-                                new TelegramClientImpl(new ApplicationPropertiesImpl())
+                                new TelegramClientImpl(applicationProperties)
                         )
                 )
         ).send();
--- src/main/java/retriever/RetrieverFactory.java
+++ src/main/java/retriever/RetrieverFactory.java
@@ -1,14 +1,38 @@
 package retriever;
 
 import http.HttpGetRequestImpl;
+import lombok.RequiredArgsConstructor;
+import properties.ApplicationProperties;
 import retriever.fantasyfootballhub.FantasyFootballHubDataSource;
 import retriever.fantasyfootballhub.FantasyFootballHubRetriever;
+import retriever.fplstatistics.FPLStatisticsDataSource;
+import retriever.fplstatistics.FPLStatisticsDataURLRetriever;
+import retriever.fplstatistics.FPLStatisticsRequestsRetriever;
+import retriever.fplstatistics.FPLStatisticsRetriever;
 
 import java.net.http.HttpClient;
 
+@RequiredArgsConstructor
 public class RetrieverFactory {
 
+    private final ApplicationProperties applicationProperties;
+
     public Retriever create() {
+        return fplStatisticsRetriever();
+    }
+
+    private FPLStatisticsRetriever fplStatisticsRetriever() {
+        return new FPLStatisticsRetriever(
+                new FPLStatisticsDataSource(
+                        new HttpGetRequestImpl(HttpClient.newHttpClient()),
+                        new FPLStatisticsDataURLRetriever(
+                                new FPLStatisticsRequestsRetriever(applicationProperties)
+                        )
+                )
+        );
+    }
+
+    private FantasyFootballHubRetriever fantasyFootballHubRetriever() {
         return new FantasyFootballHubRetriever(
                 new FantasyFootballHubDataSource(
                         new HttpGetRequestImpl(HttpClient.newHttpClient())
